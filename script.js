bf1a1729f6e0cdcccebc7d9658ceb5bba4c51ed1
Eka Hanif <ekahanif2113@gmail.com>
2022-07-22 09:06:16 +0700

    web by fazli

---
+++ script.js
@@ -0,0 +1,38 @@
+let list = document.querySelectorAll('.list');
+let itemBox = document.querySelectorAll('.itemBox');
+
+for(let i = 0; i<list.length; i++){
+    list[i].addEventListener('click', function(){
+        for(let j = 0; j<list.length; j++){
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for(let k = 0; k<itemBox.length; k++){
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+        
+        if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+            itemBox[k].classList.remove('hide');
+            itemBox[k].classList.add('active');
+        }
+    }
+    })
+}
+
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if ( this.scrollY >= 1000 ) {
+    backToTop.classList.add('show');
+
+    backToTop.addEventListener('click', () => {
+      window.scrollTo({top: 0});
+    })
+  } else {
+    backToTop.classList.remove('show');
+  }
+});
+
